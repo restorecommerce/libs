76be02cd57c02a7c9ada47aec655f91f74b04fd1
respinha <rui.espinha.ribeiro@n-fuse.co>
2018-07-06 17:53:11 +0200

    up

--- lib/core/ResourcesAPI.ts
+++ lib/core/ResourcesAPI.ts
@@ -91,12 +91,26 @@ function updateMetadata(docMeta: DocumentMetadata, newDoc: BaseDocument): BaseDo
  */
 function decodeBufferObj(document: BaseDocument, bufferFieldPath: string): BaseDocument {
   if (_.has(document, bufferFieldPath) && !_.isEmpty(_.get(document, bufferFieldPath))) {
-    const encodedBufferObj = _.get(document, bufferFieldPath).value;
+
+    let encodedBufferObj = _.get(document, bufferFieldPath);
+    if (!_.has(encodedBufferObj, 'type_url') || !_.has(encodedBufferObj, 'value')) {
+      for (let property in encodedBufferObj) {
+        if (_.has(encodedBufferObj, 'type_url') && _.has(encodedBufferObj, 'value')) {
+          encodedBufferObj = encodedBufferObj[property];
+          break;
+        }
+      }
+    }
+
+    if (_.isEmpty(encodedBufferObj.value)) {
+      return document;
+    }
     // By default it was encoded in utf8 so decoding by default from utf8
-    let decodedMsg = Buffer.from(encodedBufferObj).toString();
+    let decodedMsg = Buffer.from(encodedBufferObj.value).toString();
     // store as object in DB
     decodedMsg = JSON.parse(decodedMsg);
     _.set(document, bufferFieldPath, decodedMsg);
+
   }
   return document;
 }

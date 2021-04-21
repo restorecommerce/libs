595692f60d6d548e68ecaac1ac95a24bfc74dd39
Radu1990 <et2323@gmail.com>
2021-04-21 17:56:05 +0200

    finish batching implementation

--- packages/gql-bot/src/job_processor_gql.ts
+++ packages/gql-bot/src/job_processor_gql.ts
@@ -33,42 +33,62 @@ export class GraphQLProcessor {
           console.log('Batch size:', batchsize);
         }
 
-        let promiseArray: any;
         let counter = 0;
         let batchCounter = 0;
         let docArr: any[] = [];
+        let resultArr: any[] = [];
 
-        return new Promise<void>((resolve, reject) => {
-          yamlStream.on('data', (doc) => {
-            if (batchsize && batchsize != undefined) {
-              docArr.push(doc);
-              counter++;
-              if (counter === batchsize) {
-                yamlStream.cork();
+        // Here we read from the readable stream each yaml document parsed
+        // as an object, and if we have batching enabled we first batch this
+        // documents into smaller sets. When a dataset is ready to be imported,
+        // we pause the readable stream and emit a 'pause' event and
+        // right after that we reset the dataset object 'docArr'.
+        yamlStream.on('data', (doc) => {
+          if (batchsize && batchsize != undefined) {
+            docArr.push(doc);
+            counter++;
 
-                counter = 0;
-                batchCounter++;
-                console.log('Processing batch number:', batchCounter);
-
-                this.client.post(docArr, job);
-                docArr = [];
-
-                yamlStream.uncork();
-              }
-            } else {
-              docArr.push(doc);
-            }
-          });
-
-          yamlStream.on('end', async () => {
-            let result;
-            if (docArr && !_.isEmpty(docArr)) {
-              result = await this.client.post(docArr, job);
+            if (counter === batchsize) {
+              counter = 0;
+              batchCounter++;
+              console.log('Processing batch number:', batchCounter);
+              yamlStream.pause();
               docArr = [];
             }
-            resolve(result);
+          } else {
+            docArr.push(doc);
+          }
+        });
+
+        // On 'pause' event we create a post request using the GQL Client
+        // using the accumulated resources inside the 'docArr' dataset,
+        // we wait for the response, store the response inside an array and
+        // only then we emit a 'resume' event, resuming reading from the
+        // 'yamlStream' readable stream.
+        yamlStream.on('pause', async () => {
+          let result = await this.client.post(docArr, job);
+          resultArr.push(result);
+          yamlStream.resume();
+        });
+
+        // On 'end' if we still have data accumulated inside the 'docArr'
+        // dataset, we create a final post request to import this data as-well,
+        // store the response inside the array and finally resolve this as a
+        // Promise to return all the responses back to the initial caller.
+        return new Promise<any>((resolve, reject) => {
+          yamlStream.on('end', async () => {
+            if (docArr && !_.isEmpty(docArr)) {
+              batchCounter++;
+              console.log('Processing batch number:', batchCounter);
+              let result = await this.client.post(docArr, job);
+              resultArr.push(result);
+              docArr = [];
+
+              resolve(resultArr as any);
+            }
           });
         });
+
       }
       default: {
         throw new Error('Unsupported job operation');

29c0c30b6ee1c7f8388e3a428d4a749ea7d15065
akumar <arun.kumar@n-fuse.co>
2021-08-30 15:02:47 +0200

    fix(oidc-id-srv-integration): fix typings

--- packages/oidc-id-srv-integration/src/config.ts
+++ packages/oidc-id-srv-integration/src/config.ts
@@ -1,5 +1,5 @@
 import { createServiceConfig } from '@restorecommerce/service-config';
 // Export cfg Object
-export const cfg = createServiceConfig(process.cwd());
+export const cfg: any = createServiceConfig(process.cwd());
 // errors mapped to code and message
 export const errors = cfg.get('errors');
--- packages/oidc-id-srv-integration/src/tokenservice-adapter.ts
+++ packages/oidc-id-srv-integration/src/tokenservice-adapter.ts
@@ -1,5 +1,4 @@
 import { AdapterConstructor, Adapter, AdapterPayload } from 'oidc-provider';
-import { Logger } from 'winston';
 import { Redis } from 'ioredis';
 import { cfg } from './config';
 import * as _ from 'lodash';
@@ -55,7 +54,7 @@ interface TokenService {
 
 const delegate = (type: string) => ['AccessToken', 'RefreshToken'].includes(type);
 
-export function createIdentityServiceAdapterClass(tokenService: TokenService, logger: Logger, redisClient: Redis): AdapterConstructor {
+export function createIdentityServiceAdapterClass(tokenService: TokenService, logger: any, redisClient: Redis): AdapterConstructor {
   return class IdentityServiceAdapter implements Adapter {
 
     constructor(public type: string) { }

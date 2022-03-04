7ea221f82b6faa37df1a2f7eea83f490d96c50a5
akumar <arun.kumar@n-fuse.co>
2022-03-04 13:44:25 +0100

    fix(oidc-srv-integration): fix logger

--- packages/oidc-id-srv-integration/src/logger.ts
+++ packages/oidc-id-srv-integration/src/logger.ts
@@ -1,4 +1,12 @@
 import { createLogger } from '@restorecommerce/logger';
 import { cfg } from './config';
 
-export default createLogger(cfg.get('logger'));
+const loggerCfg = cfg.get('logger');
+if (loggerCfg) {
+  loggerCfg.esTransformer = (msg: any) => {
+    msg.fields = JSON.stringify(msg.fields);
+    return msg;
+  };
+}
+
+export default createLogger(loggerCfg);

af0af131683d270d3d7dfc2d97d4f3f96292d218
Martin <martin.yankov@n-fuse.co>
2023-05-04 12:42:45 +0200

    chore: upgrade Apollo Server 4

--- packages/facade/src/modules/ordering/gql/federation.ts
+++ packages/facade/src/modules/ordering/gql/federation.ts
@@ -1,4 +1,4 @@
-import gql from "graphql-tag";
+import { gql } from "apollo-server";
 import { buildSubgraphSchema } from "@apollo/federation";
 import { schema } from "./schema";
 import { namespace, OrderingServiceConfig } from "../interfaces";

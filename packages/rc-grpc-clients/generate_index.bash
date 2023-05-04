f3ef433341cbf757945f2380620e21463a345f9b
Gerald Baulig <gerald.baulig@n-fuse.co>
2023-05-04 14:04:06 +0200

    remove experimental index.ts for generated code

--- packages/rc-grpc-clients/generate_index.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd 'src/generated' && find '.' -type f \( -iname "*.ts" ! -iname "index.*" \) -exec bash -c 'path=$0; echo "export * as $(basename ${path%.*}) from \"${path%.*}\"";' {} \; > index.ts;
-cd '../generated-server' && find '.' -type f \( -iname "*.ts" ! -iname "index.*" \) -exec bash -c 'path=$0; echo "export * as $(basename ${path%.*}) from \"${path%.*}\"";' {} \; > index.ts;
--- packages/rc-grpc-clients/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Any } from './generated/google/protobuf/any';
-export { Any };
-
-export * from './grpc-client';
-export * as frontend from './generated'
-export * as backend from './generated-server'

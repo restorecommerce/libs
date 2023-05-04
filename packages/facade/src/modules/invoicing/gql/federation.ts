f38eab382fc437199ba738e5a4e49d6ccc775a14
Martin <martin.yankov@n-fuse.co>
2023-05-04 12:42:45 +0200

    removed: getTokenForRequest

--- packages/facade/src/modules/invoicing/gql/federation.ts
+++ packages/facade/src/modules/invoicing/gql/federation.ts
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server";
+import gql from "graphql-tag";
 import { buildSubgraphSchema } from "@apollo/federation";
 import { schema } from "./schema";
 import { namespace, InvoicingServiceConfig } from "../interfaces";

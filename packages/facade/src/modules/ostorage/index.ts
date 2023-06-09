4c553163dc84e009620871f4a6b77e4624296a12
akumar <arun.kumar@n-fuse.co>
2023-06-09 13:07:37 +0200

    chore: route for file dowload

--- packages/facade/src/modules/ostorage/index.ts
+++ packages/facade/src/modules/ostorage/index.ts
@@ -2,6 +2,9 @@ import { FederatedOstorageSchema } from './gql/federation';
 import { namespace, OstorageConfig, OstorageModule } from "./interfaces";
 import { OstorageSrvGrpcClient } from "./grpc";
 import { createFacadeModuleFactory } from "../../utils";
+import { handleGetFile } from './objectDownloadReqHandler';
+import Router from 'koa-router';
+const bodyParser = require('koa-bodyparser');
 
 export const ostorageModule = createFacadeModuleFactory<OstorageConfig, OstorageModule>(namespace, (facade, config) => {
   const ostorage = {
@@ -16,8 +19,30 @@ export const ostorageModule = createFacadeModuleFactory<OstorageConfig, Ostorage
     schema: FederatedOstorageSchema(config.config)
   });
 
+  const router = new Router();
+
+  router.use(bodyParser({ multipart: true }));
+  router.get(/^\/storage\/([^/]+)\/(.+)/, async (ctx: any, next: any) => {
+
+    const authToken = ctx.request.header['authorization'];
+    let token;
+    if (authToken && authToken.startsWith('Bearer ')) {
+      token = authToken.split(' ')[1];
+      ctx.subject = { token };
+      ctx.subject = { token };
+    }
+    const bucket = ctx.params[0];
+    const key = ctx.params[1];
+    await handleGetFile(bucket, key, ctx, ostorage.client);
+    return ctx.response;
+  });
+
+  facade.koa.use(router.routes());
+  facade.koa.use(router.allowedMethods());
+
   facade.koa.use(async (ctx, next) => {
     ctx.ostorage = ostorage;
     await next();
   });
+
 });

b84841e97862ddac989d524fd0659a1fa55751cd
Gerald Baulig <gerald.baulig@n-fuse.co>
2024-04-12 14:03:37 +0200

    fix(fulfillment_product): register fulfillment_product to facade modules

--- packages/facade/src/modules/fulfillment/grpc/index.ts
+++ packages/facade/src/modules/fulfillment/grpc/index.ts
@@ -7,18 +7,24 @@ import {
   type FulfillmentCourierServiceClient,
   FulfillmentCourierServiceDefinition
 } from '@restorecommerce/rc-grpc-clients/dist/generated/io/restorecommerce/fulfillment_courier.js';
+import { 
+  type FulfillmentProductServiceClient,
+  FulfillmentProductServiceDefinition
+} from '@restorecommerce/rc-grpc-clients/dist/generated/io/restorecommerce/fulfillment_product.js';
 import { type GrpcClientConfig } from '@restorecommerce/grpc-client';
 
 export class FulfillmentSrvGrpcClient extends RestoreCommerceGrpcClient {
 
   readonly fulfillment: FulfillmentServiceClient;
   readonly fulfillment_courier: FulfillmentCourierServiceClient;
+  readonly fulfillment_product: FulfillmentProductServiceClient;
 
   constructor(address: string, cfg: GrpcClientConfig) {
     super(address, cfg);
 
     this.fulfillment = this.createClient(cfg, FulfillmentServiceDefinition, this.channel);
     this.fulfillment_courier = this.createClient(cfg, FulfillmentCourierServiceDefinition, this.channel);
+    this.fulfillment_product = this.createClient(cfg, FulfillmentProductServiceDefinition, this.channel);
   }
 
 }

260f1d8fa5910efe0f3cfa0170788b6c233411a7
Gerald Baulig <gerald.baulig@n-fuse.co>
2024-11-06 15:26:58 +0100

    refactor(handlebars): using inline format string

--- packages/handlebars-helperized/src/helpers/l10n-helpers.ts
+++ packages/handlebars-helperized/src/helpers/l10n-helpers.ts
@@ -6,13 +6,13 @@ const localizationHandlebarsExtension = (hbs: any, opts: any) => {
     const locale = opts.locale;
     let result = opts.texts[key] || key;
     result = (typeof result === 'object') ? result[locale] : result;
-    if (!result) return 'Missing translation for ' + key;
+    if (!result) return `Missing translation for ${key}`;
     const data = hash.data.root;
     return result.replace(doubleStache, (i: any, match: any) => {
-      return data[match] ? data[match] : ('{{' + match + '}}');
+      return data[match] ? data[match] : `{{${match}}}`;
     }).replace(tripleStache, (i: any, match: any) => {
       // TODO: escaping
-      return data[match] ? data[match] : ('{{{' + match + '}}}');
+      return data[match] ? data[match] : `{{{${match}}}}`;
     });
   });
 };

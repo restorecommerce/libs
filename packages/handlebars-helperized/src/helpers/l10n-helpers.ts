31e9069e3869547fa74120241ed89ce769e3bd70
Gerald Baulig <gerald.baulig@n-fuse.co>
2025-06-23 13:32:57 +0200

    fix(helperized): l10n now allows objects with { value, default } fallback

--- packages/handlebars-helperized/src/helpers/l10n-helpers.ts
+++ packages/handlebars-helperized/src/helpers/l10n-helpers.ts
@@ -2,16 +2,17 @@ const tripleStache = /\{\{\{\s*(.*?)\s*\}\}\}/g;
 const doubleStache = /\{\{\s*(.*?)\s*\}\}/g;
 
 const localizationHandlebarsExtension = (hbs: any, opts: any) => {
-  hbs.registerHelper('t', (key: string, hash = {} as any) => {
+  hbs.registerHelper('t', (key: string, hash?: any) => {
     const locale = opts.locale;
     let result = opts.texts[key] ?? key;
     result = (typeof result === 'object') ? result[locale] : result;
+    hash = hash?.hash ?? hash;
     if (!result) return `Missing translation for ${key}`;
     return result.replace(doubleStache, (i: any, match: any) => {
-      return hash.hash[match] ?? `{{${match}}}`;
+      return hash?.[match]?.value ?? hash?.[match]?.default ?? hash?.[match] ?? `{{${match}}}`;
     }).replace(tripleStache, (i: any, match: any) => {
       // TODO: escaping
-      return hash.hash[match] ?? `{{{${match}}}}`;
+      return hash?.[match]?.value ?? hash?.[match]?.default ?? hash?.[match] ?? `{{{${match}}}}`;
     });
   });
 };

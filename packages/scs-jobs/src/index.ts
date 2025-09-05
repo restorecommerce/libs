4cc14448cc8bf7185b591def937da4ea88102cad
Gerald Baulig <gerald.baulig@n-fuse.co>
2025-09-05 14:30:01 +0200

    fix(scs-jobs): decompose error messages, eliminate lodash

--- packages/scs-jobs/src/index.ts
+++ packages/scs-jobs/src/index.ts
@@ -1,15 +1,24 @@
-import type { FilterOpts, JobType, Data } from './types.js';
+import type { FilterOpts, Data } from './types.js';
 import { createClient as createRedisClient } from 'redis';
 import type { Events } from '@restorecommerce/kafka-client';
 import type { ServiceConfig } from '@restorecommerce/service-config';
 import type { Logger } from '@restorecommerce/logger';
-import * as _ from 'lodash';
 import type { Processor } from 'bullmq';
 import { Worker } from 'bullmq';
 
+
+export const decomposeError = (err: any): {
+  code?: number,
+  message?: string,
+  details?: string,
+  stack?: Error['stack']
+} => {
+  const { code, message, details, stack } = err;
+  return { code, message, details, stack };
+}
+
 export const marshallProtobufAny = (msg: any): any => {
   return {
-    type_url: 'identity.rendering.renderRequest',
     value: Buffer.from(JSON.stringify(msg))
   };
 };
@@ -24,11 +33,12 @@ export const unmarshallProtobufAny = (msg: any, logger: any): any => {
   }
 };
 
-
-export const _filterJobData = (data: Data, encode: boolean, logger: Logger): Pick<Data, 'meta' | 'payload' | 'subject_id'> => {
-  const picked = _.pick(data, [
-    'meta', 'payload', 'subject_id'
-  ]);
+export const _filterJobData = (data: Data, encode: boolean, logger: Logger) => {
+  const picked = {
+    payload: data?.payload,
+    meta: data?.meta,
+    subject_id: data.subject_id,
+  };
 
   if (encode) {
     if (picked?.payload?.value && typeof picked.payload.value === 'string') {
@@ -47,14 +57,17 @@ export const _filterJobData = (data: Data, encode: boolean, logger: Logger): Pic
   return picked;
 };
 
-
-export const _filterQueuedJob = <T extends FilterOpts>(job: T, logger: Logger): Pick<T, 'id' | 'type' | 'data' | 'opts' | 'name'> => {
+export const _filterQueuedJob = <T extends FilterOpts>(job: T, logger: Logger) => {
   if (job && !job.type) {
     (job as any).type = (job as any).name;
   }
-  const picked: any = _.pick(job, [
-    'id', 'type', 'data', 'opts', 'name'
-  ]);
+  const picked = {
+    id: job?.id,
+    type: job?.type,
+    name: job?.name,
+    opts: job?.opts,
+    data: job?.data,
+  };
 
   if (picked?.data) {
     picked.data = _filterJobData(picked.data, false, logger);
@@ -82,7 +95,7 @@ export const runWorker = async (
   const reccurTimeCfg = cfg.get('redis');
   reccurTimeCfg.database = cfg.get('redis:db-indexes:db-reccurTime');
   const redisClient = createRedisClient(reccurTimeCfg);
-  redisClient.on('error', (err) => logger.error('Redis client error in recurring time store', err));
+  redisClient.on('error', (err) => logger.error('Redis client error in recurring time store', decomposeError(err)));
   await redisClient.connect();
 
   if ('keyPrefix' in redisConfig) {
@@ -112,10 +125,7 @@ export const runWorker = async (
             try {
               jobBufferObj = JSON.parse(filteredJob.data.payload.value.toString());
             } catch (error: any) {
-              logger.error('Error parsing job payload', {
-                code: error.code,
-                message: error.message, stack: error.stack
-              });
+              logger.error('Error parsing job payload', decomposeError(error));
             }
 
             if (!jobBufferObj) {
@@ -164,14 +174,22 @@ export const runWorker = async (
     autorun: false
   });
   worker.setMaxListeners(100); // default 10 is too low!
-  worker.on('error', err => logger.error(`worker#${queue} error`, err));
+  worker.on('error', err => logger.error(`worker#${queue} error`, decomposeError(err)));
   worker.on('closed', () => logger.verbose(`worker#${queue} closed`));
   worker.on('progress', (j, p) => logger.debug(`worker#${queue} job#${j.id} progress`, p));
   worker.on('failed', async (j, err) => {
-    logger.error(`worker#${queue} job#${j.id} failed`, err);
-    await jobEvents.emit('jobFailed', { id: j.id, schedule_type: j.data.schedule_type, error: err.message, type: j.name }).catch(err => {
-      logger.error(`Error emitting jobFailed event for ${j.name}`, err);
-    });
+    logger.error(`worker#${queue} job#${j.id} failed`, decomposeError(err));
+    await jobEvents.emit(
+      'jobFailed', 
+      {
+        id: j.id,
+        schedule_type: j.data.schedule_type,
+        error: err.message,
+        type: j.name }
+      ).catch(err => {
+        logger.error(`Error emitting jobFailed event for ${j.name}`, decomposeError(err));
+      }
+    );
   });
   worker.on('closing', msg => logger.verbose(`worker#${queue} closing: ${msg}`));
   worker.on('completed', j => logger.info(`worker#${queue} job#${j.id} completed`));
@@ -181,7 +199,7 @@ export const runWorker = async (
   worker.on('ready', () => logger.verbose(`worker#${queue} ready`));
   worker.on('resumed', () => logger.verbose(`worker#${queue} resumed`));
 
-  worker.run().catch(err => logger.error(`worker#${queue} run error`, err));
+  worker.run().catch(err => logger.error(`worker#${queue} run error`, decomposeError(err)));
   await worker.waitUntilReady();
   return worker;
 };
